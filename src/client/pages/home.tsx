67348fd5754bb761df8acfb0cdd0868ab872af99
Eduardo Gonzalez Zangarini <26151069+eduardogz94@users.noreply.github.com>
2023-01-29 23:41:49 +0100

    test: fix workflow for docs activable only on changes in components or pages folder

--- src/client/pages/home.tsx
+++ src/client/pages/home.tsx
@@ -27,17 +27,6 @@ const Home: NextPage = () => {
                 database and authentication.
               </div>
             </Link>
-            <Link
-              className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
-              href="https://create.t3.gg/en/introduction"
-              target="_blank"
-            >
-              <h3 className="text-2xl font-bold">Documentation →</h3>
-              <div className="text-lg">
-                Learn more about Create T3 App, the libraries it uses, and how
-                to deploy it.
-              </div>
-            </Link>
           </div>
         </div>
       </main>
