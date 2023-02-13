7ee0d76a58ee623567b5b9f2dc613ad204c97867
Eduardo Gonzalez Zangarini <26151069+eduardogz94@users.noreply.github.com>
2023-02-13 21:46:39 +0100

    feat: adding initial readme

--- jest.config.ts
+++ jest.config.ts
@@ -2,14 +2,14 @@ import type { Config } from '@jest/types';
 
 const config: Config.InitialOptions = {
   moduleFileExtensions: ['js', 'json', 'ts'],
-  modulePaths: ['.'],
+  modulePaths: ['./src/server/'],
   testRegex: '.*\\.spec\\.ts$',
   transform: {
     '^.+\\.(t|j)s$': 'ts-jest',
   },
   collectCoverageFrom: ['src/server/**/*.(t|j)s'],
   coveragePathIgnorePatterns: ['src/server/console', 'src/server/migration'],
-  coverageDirectory: 'coverage',
+  coverageDirectory: 'src/server/coverage',
   testEnvironment: 'node',
 };
 
