d68308e9bfd9a9547ce7b2acfef2e6fdcc805d63
Eduardo Gonzalez <ezangarini@hotmail.com>
2023-02-14 01:44:11 +0100

    feat: adding initial readme (#25)
    * test: added tests to useLocalStorage {WIP FOR THE MODULE}
    * test: fixing tests setup

--- src/client/jest.config.js
+++ src/client/jest.config.js
@@ -1,9 +1,23 @@
-module.exports = {
+/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
+const nextJest = require("next/jest");
+
+const createJestConfig = nextJest({
+  // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
+  dir: "./",
+});
+
+// Add any custom config to be passed to Jest
+const customJestConfig = {
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
+  moduleNameMapper: {
+    "\\.(css|less|scss)$": "./__mocks__/styleMock.js",
+    "\\.(gif|ttf|eot|svg)$": "./__mocks__/fileMock.js",
+  },
+  testEnvironment: "jest-environment-jsdom",
   verbose: true,
   preset: "ts-jest",
-  testEnvironment: "jest-environment-jsdom",
-  moduleNameMapper: {
-    "\\.(css|less|scss)$": "<rootDir>/__mocks__/styleMock.js",
-    "\\.(gif|ttf|eot|svg)$": "<rootDir>/__mocks__/fileMock.js",
-  },
+  coverageReporters: ["clover", "json", "lcov", ["text", { skipFull: true }]],
 };
+
+// createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
+module.exports = createJestConfig(customJestConfig);
