5f62028d62c8b8a4d4ef200d908aa39a00058c59
Eduardo Gonzalez Zangarini <26151069+eduardogz94@users.noreply.github.com>
2023-02-14 01:12:03 +0100

    fix: deleted .only on tests

--- src/client/jest.config.js
+++ src/client/jest.config.js
@@ -1,11 +1,23 @@
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
-module.exports = {
-  verbose: true,
-  preset: "ts-jest",
-  testEnvironment: "jest-environment-jsdom",
-  coverageReporters: ["clover", "json", "lcov", ["text", { skipFull: true }]],
+const nextJest = require("next/jest");
+
+const createJestConfig = nextJest({
+  // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
+  dir: "./",
+});
+
+// Add any custom config to be passed to Jest
+const customJestConfig = {
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
   moduleNameMapper: {
-    "\\.(css|less|scss|tsx)$": "./__mocks__/styleMock.js",
+    "\\.(css|less|scss)$": "./__mocks__/styleMock.js",
     "\\.(gif|ttf|eot|svg)$": "./__mocks__/fileMock.js",
   },
+  testEnvironment: "jest-environment-jsdom",
+  verbose: true,
+  preset: "ts-jest",
+  coverageReporters: ["clover", "json", "lcov", ["text", { skipFull: true }]],
 };
+
+// createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
+module.exports = createJestConfig(customJestConfig);
